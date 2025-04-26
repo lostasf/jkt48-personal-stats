31fadcb271193454f0d7d0ef282ebf545e3c7d1d
Ian <alfian101@gmail.com>
2025-04-27 05:12:49 +0700

    add test api

--- api/index.py
+++ api/index.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+import requests
+from bs4 import BeautifulSoup
 
 ### Create FastAPI instance with custom docs and openapi url
 app = FastAPI(docs_url="/api/py/docs", openapi_url="/api/py/openapi.json")
 
+def getCalendarData():
+    response = requests.get("https://jkt48.com/calendar/list/y/2025/m/4/d/1?lang=id")
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup
+    else:
+        return None
+
 @app.get("/api/hello")
-def hello_fast_api():
-    return {"message": "Hello from FastAPI"}
+def getCalendarPage():
+    soup = getCalendarData()
+    return soup.prettify() if soup else {"error": "Failed to retrieve data"}
