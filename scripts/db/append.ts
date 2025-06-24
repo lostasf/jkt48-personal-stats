6e67ed53a02c39a03f9e487f1360542d9e8cc1bb
Ian <alfian101@gmail.com>
2025-06-24 23:58:30 +0700

    dev: commented unused runtime code

--- scripts/db/append.ts
+++ scripts/db/append.ts
@@ -1,41 +1,41 @@
-import { getOneScheduleByDateShowTimeAndName, createTicketHistoryByScheduleId } from '@/db/queries'
+// import { getOneScheduleByDateShowTimeAndName, createTicketHistoryByScheduleId } from '@/db/queries'
 
-import { IndonesianHumanReadableDate } from '@/types/date'
-import { TicketShowTime } from '@/types/scrape'
+// import { IndonesianHumanReadableDate } from '@/types/date'
+// import { TicketShowTime } from '@/types/scrape'
 
-import data from '@/json/ticketHistory.json'
-import { TicketHistory, TicketType, TicketOperation } from '@/db/types'
+// import data from '@/json/ticketHistory.json'
+// import { TicketHistory, TicketType, TicketOperation } from '@/db/types'
 
-const main = async () => {
-    try {
-        const mappedTicketHistories: Omit<TicketHistory, 'id'>[] = []
+// const main = async () => {
+//     try {
+//         const mappedTicketHistories: Omit<TicketHistory, 'id'>[] = []
 
-        await data.reduce(async (promise, ticketHistory) => {
-            await promise
-            const scheduleId = await getOneScheduleByDateShowTimeAndName(
-                ticketHistory.showDate as IndonesianHumanReadableDate,
-                ticketHistory.showTime as TicketShowTime,
-                ticketHistory.name as string
-            )
+//         await data.reduce(async (promise, ticketHistory) => {
+//             await promise
+//             const scheduleId = await getOneScheduleByDateShowTimeAndName(
+//                 ticketHistory.showDate as IndonesianHumanReadableDate,
+//                 ticketHistory.showTime as TicketShowTime,
+//                 ticketHistory.name as string
+//             )
 
-            if (!scheduleId) throw new Error(`No schedule found in the database. ${scheduleId} ${ticketHistory.name} ${ticketHistory.showDate} ${ticketHistory.showTime}`)
+//             if (!scheduleId) throw new Error(`No schedule found in the database. ${scheduleId} ${ticketHistory.name} ${ticketHistory.showDate} ${ticketHistory.showTime}`)
 
-            mappedTicketHistories.push({
-                operation: ticketHistory.operation as TicketOperation,
-                ticketType: ticketHistory.ticketType as TicketType,
-                url: ticketHistory.detailUrl?.replace('https://jkt48.com', ''),
-                scheduleId: scheduleId.id,
-            })
-        }, Promise.resolve())
+//             mappedTicketHistories.push({
+//                 operation: ticketHistory.operation as TicketOperation,
+//                 ticketType: ticketHistory.ticketType as TicketType,
+//                 url: ticketHistory.detailUrl?.replace('https://jkt48.com', ''),
+//                 scheduleId: scheduleId.id,
+//             })
+//         }, Promise.resolve())
 
-        const res = await createTicketHistoryByScheduleId(mappedTicketHistories)
+//         const res = await createTicketHistoryByScheduleId(mappedTicketHistories)
 
-        console.log('Ticket history created successfully:', res)
+//         console.log('Ticket history created successfully:', res)
 
-    } catch (error) {
-        console.error(error)
-        throw new Error('Failed to run ')
-    }
-}
+//     } catch (error) {
+//         console.error(error)
+//         throw new Error('Failed to run ')
+//     }
+// }
 
-void main()
+// void main()
